23ce03e8987115095b36d54a0c6b0026353760b3
Iann <iisackssonoliveira@gmail.com>
2020-06-05 20:58:10 -0300

    feat: Creating database

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -11,15 +11,22 @@ import {
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity()
 class Order {
+  @PrimaryGeneratedColumn()
   id: string;
 
+  @OneToMany(() => Customer, customer => customer.id)
   customer: Customer;
 
+  @OneToOne(() => OrdersProducts, ordersProducts => ordersProducts.order)
+  @JoinColumn()
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
